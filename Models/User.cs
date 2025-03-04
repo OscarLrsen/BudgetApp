00a3e9a721fb50f23a07e1f3b0134198e9a129a9
Ture Christenson <ture.c@hotmail.com>
2025-03-04 10:52:38 +0100

    Lade till kommentarer + ändrade i mainpage och homepage

--- Models/User.cs
+++ Models/User.cs
@@ -4,6 +4,7 @@ namespace BudgetApp.Models
 {
     public class User : IdentityUser
     {
+        //Lägg till här om det behövs fler properties i User!!
         public int TransactionId { get; set; }
         public List<Transaction> Transactions { get; set; } = new(); // Navigation property, gör det lättare att hämta data
 
